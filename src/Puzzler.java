efc0f1d9b3231956b3875fe5b8de5e494bdf9b53
xfarkasp <xfarkasp@stuba.sk>
2023-10-05 23:02:08 +0200

    development of the puzzler class

--- src/Puzzler.java
+++ src/Puzzler.java
@@ -1,20 +1,95 @@
+import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 
 public class Puzzler {
 
     private PuzzleNode root;
+    final int unsolvableLimit = 1000000;
+    private int numberOfNodes;
+    private ArrayList<ArrayList<Integer>> puzzleFinal;
+    private ArrayList<PuzzleNode> uniqueNodes;
 
-    public Puzzler() {
-        this.root = new PuzzleNode();
+
+    public Puzzler(ArrayList<ArrayList<Integer>> puzzleStart, ArrayList<ArrayList<Integer>> puzzleFinal) {
+        this.numberOfNodes = 0;
+        this.puzzleFinal = puzzleFinal;
+        this.root = new PuzzleNode(puzzleStart, getErrCount(puzzleStart, puzzleFinal));
     }
 
-    boolean validator(ArrayList<Integer> startPos, ArrayList<Integer> endPosition){
+    public int getErrCount(ArrayList<ArrayList<Integer>> puzzleStatus, ArrayList<ArrayList<Integer>> puzzleFinal){
+        int nokPosCount = 0;
+        //compare the value on the same position from the current status to the same position at final status of the puzzle
+        for(int i = 0; i < puzzleStatus.size() && i < puzzleFinal.size(); i++){
+            for(int j = 0; j < puzzleStatus.get(i).size() && j < puzzleFinal.size(); j++){
+                if(puzzleStatus.get(i).get(j) != puzzleFinal.get(i).get(j))
+                    nokPosCount++;
+            }
+        }
+        return nokPosCount;
+    }
 
+    boolean validator(ArrayList<ArrayList<Integer>> startPos, ArrayList<ArrayList<Integer>> endPosition){
+        if(startPos == endPosition)
+            return true;
         return false;
     }
 
-    void puzzleSolver(ArrayList<Integer> startPos, ArrayList<Integer> endPosition){
+    boolean isUnique(ArrayList<ArrayList<Integer>> puzzleStatus){
+        for(PuzzleNode node: uniqueNodes){
+            if(node.puzzleStatus == puzzleStatus)
+                return true;
+        }
+        return false;
+    }
 
+    void puzzleSolver(ArrayList<ArrayList<Integer>> startPos, ArrayList<ArrayList<Integer>> endPosition){
+        PuzzleNode currentNode = root;
+        while (numberOfNodes != unsolvableLimit){
+            int bigestGreed = -1;
+            PuzzleNode nextNode = null;
+            //variation 1
+            ArrayList<ArrayList<Integer>> alteredPuzzle = oneUp(currentNode.puzzleStatus);
+            if(alteredPuzzle != null){
+                PuzzleNode variation1 = new PuzzleNode(alteredPuzzle, getErrCount(currentNode.puzzleStatus, this.puzzleFinal));
+                if(isUnique(variation1.puzzleStatus)) {
+                    uniqueNodes.add(variation1);
+                    currentNode.child1 = variation1;
+                    if(currentNode.child1.greedCount >= bigestGreed){
+                        bigestGreed = currentNode.child1.greedCount;
+                        nextNode = currentNode.child1;
+                    }
+                }
+            }
+            //variation 2
+            alteredPuzzle = oneDown(currentNode.puzzleStatus);
+            if(alteredPuzzle != null){
+                PuzzleNode variation2 = new PuzzleNode(alteredPuzzle, getErrCount(currentNode.puzzleStatus, this.puzzleFinal));
+                if(isUnique(variation2.puzzleStatus)) {
+                    uniqueNodes.add(variation2);
+                    currentNode.child2 = variation2;
+                }
+            }
+            //variation 3
+            alteredPuzzle = oneRight(currentNode.puzzleStatus);
+            if(alteredPuzzle != null){
+                PuzzleNode variation3 = new PuzzleNode(alteredPuzzle, getErrCount(currentNode.puzzleStatus, this.puzzleFinal));
+                if(isUnique(variation3.puzzleStatus)) {
+                    uniqueNodes.add(variation3);
+                    currentNode.child3 = variation3;
+                }
+            }
+            //variation 4
+            alteredPuzzle = oneLeft(currentNode.puzzleStatus);
+            if(alteredPuzzle != null){
+                PuzzleNode variation4 = new PuzzleNode(alteredPuzzle, getErrCount(currentNode.puzzleStatus, this.puzzleFinal));
+                if(isUnique(variation4.puzzleStatus)) {
+                    uniqueNodes.add(variation4);
+                    currentNode.child4 = variation4;
+                }
+            }
+
+        }
     }
     //index 0 = row, index 1 = col
     ArrayList<Integer> emptyPosition(ArrayList<ArrayList<Integer>> puzzleStatus){
@@ -31,36 +106,75 @@ public class Puzzler {
         return positions;
     }
 
-    void oneUp(ArrayList<ArrayList<Integer>> puzzleStatus){
+    public ArrayList<ArrayList<Integer>> oneUp(ArrayList<ArrayList<Integer>> puzzleStatus){
+        //row and col position
         ArrayList<Integer> positions = emptyPosition(puzzleStatus);
-
-        //oneUp
+        //if zero is in top row
         if(positions.get(0) == 0){
-            int tmp = puzzleStatus.get(puzzleStatus.size()-1).get(positions.get(1)); //get the value at the same position in the last row as is zero at first row
-            puzzleStatus.get(0).set(positions.get(1), tmp); //set the previous position of zero, to value of tmp
-            puzzleStatus.get(puzzleStatus.size()-1).set(positions.get(1), 0);
+            return null;
         }
         else{
             int tmp = puzzleStatus.get(positions.get(0) - 1).get(positions.get(1));
             puzzleStatus.get(positions.get(0)).set(positions.get(1), tmp); //set the previous position of zero, to value of tmp
             puzzleStatus.get(positions.get(0) - 1).set(positions.get(1), 0);
         }
+        return puzzleStatus;
     }
-    void oneDown(ArrayList<Integer> position){}
-    void oneLeft(ArrayList<Integer> position){}
-    void oneRight(ArrayList<Integer> position){}
+    public ArrayList<ArrayList<Integer>> oneDown(ArrayList<ArrayList<Integer>> puzzleStatus){
+        //row and col position
+        ArrayList<Integer> positions = emptyPosition(puzzleStatus);
+        //if zero is in top row
+        if(positions.get(0) == puzzleStatus.size()-1){
+            return null;
+        }
+        else{
+            int tmp = puzzleStatus.get(positions.get(0) + 1).get(positions.get(1));
+            puzzleStatus.get(positions.get(0)).set(positions.get(1), tmp); //set the previous position of zero, to value of tmp
+            puzzleStatus.get(positions.get(0) + 1).set(positions.get(1), 0);
+        }
+        return puzzleStatus;
+    }
+    public ArrayList<ArrayList<Integer>> oneRight(ArrayList<ArrayList<Integer>> puzzleStatus){
+        //row and col position
+        ArrayList<Integer> positions = emptyPosition(puzzleStatus);
+        //if zero is in top row
+        if(positions.get(1) == puzzleStatus.get(positions.get(0)).size() - 1){
+            return null;
+        }
+        else{
+            int tmp = puzzleStatus.get(positions.get(0)).get(positions.get(1));
+            puzzleStatus.get(positions.get(0)).set(positions.get(1), tmp); //set the previous position of zero, to value of tmp
+            puzzleStatus.get(positions.get(0)).set(positions.get(1) + 1, 0);
+        }
+        return puzzleStatus;
+    }
+    public ArrayList<ArrayList<Integer>> oneLeft(ArrayList<ArrayList<Integer>> puzzleStatus){
+        //row and col position
+        ArrayList<Integer> positions = emptyPosition(puzzleStatus);
+        //if zero is in top row
+        if(positions.get(1) == puzzleStatus.get(positions.get(0)).size() - 1){
+            return null;
+        }
+        else{
+            int tmp = puzzleStatus.get(positions.get(0)).get(positions.get(1));
+            puzzleStatus.get(positions.get(0)).set(positions.get(1), tmp); //set the previous position of zero, to value of tmp
+            puzzleStatus.get(positions.get(0)).set(positions.get(1) + 1, 0);
+        }
+        return puzzleStatus;
+    }
 }
 
 class PuzzleNode {
-    int errNum;
-    protected ArrayList<Integer> currStateOfPuzzle;
-    protected ArrayList<PuzzleNode> currLvl;
-    protected ArrayList<PuzzleNode> nextLvl;
+    int greedCount;
 
-    protected PuzzleNode() {
-        this.errNum = errNum;
-        this.currStateOfPuzzle = new ArrayList<>();
-        this.currLvl = new ArrayList<>();
-        this.nextLvl = new ArrayList<>();
+    PuzzleNode child1;
+    PuzzleNode child2;
+    PuzzleNode child3;
+    PuzzleNode child4;
+    protected ArrayList<ArrayList<Integer>> puzzleStatus;
+
+    protected PuzzleNode(ArrayList<ArrayList<Integer>> puzzleStatus, int greedCount) {
+        this.greedCount = greedCount;
+        this.puzzleStatus = puzzleStatus;
     }
 }
